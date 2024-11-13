29756efd5c9cf544ea6204f80779fb72e2e8f46a
ImagoXV <arthur.cousson@ens.fr>
2024-11-13 18:34:51 +0100

    Database testing

---
+++ config/Database_testing/Database_testing.R
@@ -0,0 +1,11 @@
+setwd("~/Documents/Thesis/NanoASV/Software_dev/NanoASV/")
+
+load("COMPLETE_ref_Mock_run_OUTPUT/Results/Rdata/NanoASV.rdata")
+
+COMPLETE.phy <- NanoASV
+
+load("UNCOMPLETE_ref_Mock_run_OUTPUT/Results/Rdata/NanoASV.rdata")
+
+UNCOMPLETE.phy <- NanoASV
+
+
