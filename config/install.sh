9efa571fa24a5c7aca72e20c1411e12ed01f804b
ImagoXV <arthur.cousson@ens.fr>
2025-04-18 10:15:03 +0200

    install.sh modification

--- config/install.sh
+++ config/install.sh
@@ -2,12 +2,13 @@
 INSTALL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $INSTALL_DIR/../
 VERSION="$(head -n1 environment.yml | cut -f2 -d " ")"
-mkdir conda
 conda env create --file environment.yml -p $VERSION
-ACTIVATE_DIR=$(conda env list | grep -w 'NanoASV' | awk '{print $2}')/etc/conda/activate.d
+ACTIVATE_DIR="$INSTALL_DIR/../$VERSION/etc/conda/activate.d"
+#ACTIVATE_DIR=$(conda env list | grep -w 'NanoASV' | awk '{print $2}')/etc/conda/activate.d
 cp config/alias.sh $ACTIVATE_DIR/
 cp config/paths.sh $ACTIVATE_DIR/
 echo "export NANOASV_PATH=$(pwd)" >> $ACTIVATE_DIR/paths.sh
-DEACTIVATE_DIR=$(conda env list | grep -w 'NanoASV' | awk '{print $2}')/etc/conda/deactivate.d
+DEACTIVATE_DIR="$INSTALL_DIR/../$VERSION/etc/conda/activate.d"
+#DEACTIVATE_DIR=$(conda env list | grep -w 'NanoASV' | awk '{print $2}')/etc/conda/deactivate.d
 cp config/unalias.sh $DEACTIVATE_DIR/
 chmod +x workflow/run.sh
