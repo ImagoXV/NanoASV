d983cafcf416fc2208e9e1217b66a3fb6980ab2b
ImagoXV <arthur.cousson@ens.fr>
2025-04-18 10:00:32 +0200

    Add $INSTALL_dir variable to properly locate NanoASV

--- config/install.sh
+++ config/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd ~/NanoASV
-conda env create --file environment.yml
+INSTALL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $INSTALL_DIR
+mkdir conda
+conda env create --file environment.yml -p conda
 ACTIVATE_DIR=$(conda env list | grep -w 'NanoASV' | awk '{print $2}')/etc/conda/activate.d
 cp config/alias.sh $ACTIVATE_DIR/
 cp config/paths.sh $ACTIVATE_DIR/
