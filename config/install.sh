4180ca4d2aed466334c6a015bcb0915291b72b83
ImagoXV <arthur.cousson@ens.fr>
2025-04-18 10:04:20 +0200

    install.sh modification

--- config/install.sh
+++ config/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 INSTALL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd $INSTALL_DIR
+cd $INSTALL_DIR/../
 mkdir conda
 conda env create --file environment.yml -p conda
 ACTIVATE_DIR=$(conda env list | grep -w 'NanoASV' | awk '{print $2}')/etc/conda/activate.d
